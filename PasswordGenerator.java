b5633f88db7ea38d72f8a1aad9a5721a9cf415bc
Angcroft <svenegas533@gmail.com>
2024-05-30 10:31:02 -0400

    Separated all but main in another file

---
+++ PasswordGenerator.java
@@ -0,0 +1,41 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String UPPER = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+    private static final String ALL_CHARACTERS = UPPER + LOWER + DIGITS + SPECIAL;
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static String generatePassword(int length) {
+        if (length < 12) {
+            throw new IllegalArgumentException("Password length should be at least 12 characters");
+        }
+
+        StringBuilder password = new StringBuilder(length);
+        password.append(UPPER.charAt(RANDOM.nextInt(UPPER.length())));
+        password.append(LOWER.charAt(RANDOM.nextInt(LOWER.length())));
+        password.append(DIGITS.charAt(RANDOM.nextInt(DIGITS.length())));
+        password.append(SPECIAL.charAt(RANDOM.nextInt(SPECIAL.length())));
+
+        for (int i = 4; i < length; i++) {
+            password.append(ALL_CHARACTERS.charAt(RANDOM.nextInt(ALL_CHARACTERS.length())));
+        }
+
+        return shuffleString(password.toString());
+    }
+
+    private static String shuffleString(String input) {
+        char[] characters = input.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            int randomIndex = RANDOM.nextInt(characters.length);
+            char temp = characters[i];
+            characters[i] = characters[randomIndex];
+            characters[randomIndex] = temp;
+        }
+
+        return new String(characters);
+    }
+}
